a5e386a72be54b962abdd516d579d91aa4ccb409
hodee <tlsghk8619@gmail.com>
2023-03-07 16:37:49 +0900

    while

---
+++ 1074.py
@@ -0,0 +1,22 @@
+N, x, y = map(int, input().split())
+
+num = 0 
+while N != 0:
+    N -= 1
+    if x < 2 **N and y < 2**N:
+        num+=0
+        
+    elif x < 2 **N and y >= 2**N:
+        num+= (2**N) * (2**N) *1
+        y -=2**N
+        
+    elif x >= 2 **N and y < 2**N:
+        num+= (2**N) * (2**N) *2
+        x -=2**N
+        
+    elif x >= 2 **N and y >= 2**N:
+        num+= (2**N) * (2**N) *3
+        x -=2**N
+        y -=2**N
+        
+print(num)
